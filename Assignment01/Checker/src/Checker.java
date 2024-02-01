afc79d60550c808bd6948577f68f54562efb01a6
Hossein Tarokh <mhtarokh@gmail.com>
2024-02-01 20:51:29 +0100

    Almost finished. It controls if the move in in the field, in the right direction, if it is moving only one row. Now we just must check the diagonality in the X direction, that is easy, ABS(X_OLD X-New)==1.

--- Assignment01/Checker/src/Checker.java
+++ Assignment01/Checker/src/Checker.java
@@ -1,3 +1,6 @@
+import com.sun.source.tree.ReturnTree;
+import java.lang.Math;
+
 import java.util.Scanner;
 
 public class Checker {
@@ -57,24 +60,79 @@ public class Checker {
         return new byte[]{x,y};
     }
 
-    public static boolean isLocationValid(char playerTurn, char[][] playGround,byte[] oldLocation) {
-        if(oldLocation[0]<8 && oldLocation[0]>=0){
-            System.out.println("X in the field");
-            if(oldLocation[1]<8 && oldLocation[1]>=0){
-                System.out.println("Y in the field");
-                if(playGround[oldLocation[1]][oldLocation[0]] == playerTurn){
-                    System.out.println("location is valid.");
-                    return true;
-                } else {
-                    System.out.println("location is not valid.");
-                    return false;
-                }
+    public static boolean isLocationInTheField(byte[] location) {
+        if (location[0] > 8 || location[0] < 0) {
+            return false;
+        }
+        if (location[1] > 8 || location[1] < 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public static boolean isLocationEmpty(byte[] location, char[][] playGround) {
+        return playGround[location[1]][location[0]] == '0';
+    }
+
+    public static boolean isPlayerMovingInTheRightDirection(byte[] oldLocation, byte[] newLocation, char playerTurn) {
+        if(playerTurn == '1'){
+            //Player 1 must move down, therefore the new_Y = old_Y+1
+            return newLocation[1] - oldLocation[1] > 0;
+        } else {
+            //Player 2 must move up, therefore the new_Y = old_Y-1
+            return newLocation[1] - oldLocation[1] < 0;
+        }
+    }
+
+    public static boolean isPlayerMovingOneRow(byte[] oldLocation, byte[] newLocation) {
+            //Player 1 must move down, therefore the new_Y = old_Y+1
+            return  Math.abs(newLocation[1] - oldLocation[1]) == 1;
+    }
+
+    //Check if the location is valid. While checking the old location, the location is valid if the current user has a stone in that location.
+    public static boolean isOldLocationValid(char playerTurn, char[][] playGround,byte[] oldLocation) {
+        //Check if the entered location is in the field.
+        if(isLocationInTheField(oldLocation)){
+            if(playGround[oldLocation[1]][oldLocation[0]] == playerTurn){
+                System.out.println(" ");
+                //The location is valid
+                return true;
             } else {
-                System.out.println("Y out of the field.");
+                System.out.println("You do not have a stone in that location. Try again.");
                 return false;
             }
         } else {
-            System.out.println("X out of the field.");
+            System.out.println("The entered location is out of the field. Try again.");
+            return false;
+        }
+    }
+
+    public static boolean isNewLocationValid(char playerTurn, char[][] playGround,byte[] oldLocation, byte[] newLocation) {
+        //Check if the entered location is in the field.
+        if(isLocationInTheField(newLocation)){
+            //Check if the new location is empty
+            if(isLocationEmpty(newLocation, playGround)) {
+                //Check if the player is moving in the right direction
+                if (isPlayerMovingInTheRightDirection(oldLocation, newLocation, playerTurn)) {
+                    //Check if the player is moving only one row.
+                    if(isPlayerMovingOneRow(oldLocation, newLocation)){
+                        System.out.println("In the field, empty, right direction, one row. Now we have to check if it is diagonal.");
+                        return true;
+                    } else {
+                        System.out.println("You are moving more than one row.");
+                        return false;
+                    }
+                } else {
+                    System.out.println("You are moving in the wrong direction.");
+                    return false;
+                }
+            } else {
+                System.out.println("The new location is not empty.");
+                return false;
+            }
+        } else {
+            System.out.println("The entered location is out of the field. Try again.");
             return false;
         }
     }
@@ -91,6 +149,7 @@ public class Checker {
         String lastLine = "   0 1 2 3 4 5 6 7";
         char[][] playGround = new char[8][8];
         boolean isOldLocationValid = false;
+        boolean isNewLocationValid = false;
 
         //initializing the game
         playGround = initPlayGround();
@@ -102,12 +161,22 @@ public class Checker {
             //identify which user turn it is
             System.out.printf("Turn of player no.%s %n",playerTurn);
 
+            byte[] oldLocation = new byte[2];
+            //...............OLD................
             //get the coordination of the piece to move
             while(!isOldLocationValid){
                 System.out.println("Coordinate of piece to move");
-                byte[] oldLocation = getLocation();
-                isOldLocationValid = isLocationValid(playerTurn,playGround,oldLocation);
-                if(!isOldLocationValid){
+                oldLocation = getLocation();
+                isOldLocationValid = isOldLocationValid(playerTurn,playGround,oldLocation);
+            }
+
+            //****************NEW****************
+            //Get the destination location
+            while(!isNewLocationValid){
+                System.out.println("Coordinate of new position");
+                byte[] newLocation = getLocation();
+                isNewLocationValid = isNewLocationValid(playerTurn, playGround, oldLocation, newLocation);
+                if(!isNewLocationValid){
                     System.out.println("You entered an invalid location, try again.");
                 }
             }
